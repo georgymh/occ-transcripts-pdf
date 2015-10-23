1deefb7a32b3d6d6ef55597ab4839fb14ea0fedc
Georgy Marrero <georgymarrero@gmail.com>
2015-10-23 16:40:09 -0700

    Add PDF Generator + Composer Dependency

---
+++ pdfMyTranscripts.php
@@ -0,0 +1,21 @@
+<?php
+
+// somewhere early in your project's loading, require the Composer autoloader
+// see: http://getcomposer.org/doc/00-intro.md
+require 'vendor/autoload.php';
+// disable DOMPDF's internal autoloader if you are using Composer
+define('DOMPDF_ENABLE_AUTOLOAD', false);
+// include DOMPDF's default configuration
+require_once 'vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+// BODY
+if (isset($_POST["html"])) {
+	$html = $_POST["html"];
+
+	$dompdf = new DOMPDF();
+	$dompdf->load_html($html);
+	$dompdf->render();
+	$dompdf->stream("transcripts.pdf");	
+}
+
+?>
