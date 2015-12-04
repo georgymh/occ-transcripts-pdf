b1e124b091e6ccd8c9530536cad5c36164270515
Georgy Marrero <georgymarrero@gmail.com>
2015-12-03 19:22:56 -0800

    Refactor transcripts getter
    Note: Still have to put this inside its own class. It is till messy.

--- getMyTranscripts.php
+++ getMyTranscripts.php
@@ -25,182 +25,82 @@ $cookie_file = fopen($cookie_file_path, "w");
 
 // 1.a. Log into MyCoast.
 
-// Set up POST info with Milliseconds.
-$postinfo = "user=".$username."&pass=".$password."&uuid=".$milliseconds;
+$loginInfo = "user=".$username."&pass=".$password."&uuid=".$milliseconds; // Set up POST info with Milliseconds.
+$url = 'https://mycoast.cccd.edu/cp/home/login';
+$options = [
+  'post' => $loginInfo,
+  'referer' => 'https://mycoast.cccd.edu/cp/home/displaylogin',
+  'host' => 'mycoast.cccd.edu:443',
+  'follow_location' => false
+];
 
-// Perform the curl request.
-$url="https://mycoast.cccd.edu/cp/home/login";
-$c = curl_init();
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_URL, $url);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_REFERER, 'https://mycoast.cccd.edu/cp/home/displaylogin');
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_setopt($c, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($c, CURLOPT_POST, 1);
-curl_setopt($c, CURLOPT_POSTFIELDS, $postinfo);
-$loginResponse = curl_exec($c);
-curl_close($c);
+$data = performRequest($url, $cookie_file_path, $options);
+unset($options['post']);
 
-if ( strpos($loginResponse, '<title>Error: Failed Login</title>') == true) {
+// Check if login was successful.
+if ( strpos($data['response'], '<title>Error: Failed Login</title>') == true ) {
 	echo 0;
 	exit();
 }
 
 // 1.b. Log into MyCoast.
-$c = curl_init('https://mycoast.cccd.edu/cp/home/loginok.html');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_REFERER, 'https://mycoast.cccd.edu/cp/home/displaylogin');
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($c);
-curl_close($c);
+$url = 'https://mycoast.cccd.edu/cp/home/loginok.html';
+$data = performRequest($url, $cookie_file_path, $options);
+unset($options['referer']);
 
 // 1.c. Log into MyCoast.
-$c = curl_init('https://mycoast.cccd.edu/cp/home/next');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($c);
-curl_close($c);
+$url = 'https://mycoast.cccd.edu/cp/home/next';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 1.d. Finish Login into MyCoast.
-$c = curl_init('https://mycoast.cccd.edu/render.userLayoutRootNode.uP?uP_root=root');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($c);
-curl_close($c);
+$url = 'https://mycoast.cccd.edu/render.userLayoutRootNode.uP?uP_root=root';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 2. Access MyCoast Student Tab.
-$c = curl_init('https://mycoast.cccd.edu/tag.1644b8f9cbdfde0f.render.userLayoutRootNode.uP?uP_root=root&uP_sparam=activeTab&activeTab=u12l1s2&uP_tparam=frm&frm=');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($c);
-curl_close($c);
+$url = 'https://mycoast.cccd.edu/tag.1644b8f9cbdfde0f.render.userLayoutRootNode.uP?uP_root=root&uP_sparam=activeTab&activeTab=u12l1s2&uP_tparam=frm&frm=';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 3.a. Access Unofficial Transcript Term Selector.
-$c = curl_init('https://mycoast.cccd.edu/cp/render.UserLayoutRootNode.uP?uP_tparam=utf&utf=https%3A%2F%2Fmycoast.cccd.edu%2Fcp%2Fip%2Flogin%3Fsys%3Dsctssb%26url%3Dhttps%3A%2F%2Fbannerlsp.cccd.edu%2Fpls%2FPROD%2Fbwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_exec($c);
-curl_close($c);
+$url = 'https://mycoast.cccd.edu/cp/render.UserLayoutRootNode.uP?uP_tparam=utf&utf=https%3A%2F%2Fmycoast.cccd.edu%2Fcp%2Fip%2Flogin%3Fsys%3Dsctssb%26url%3Dhttps%3A%2F%2Fbannerlsp.cccd.edu%2Fpls%2FPROD%2Fbwskotrn.P_ViewTermTran';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 3.b. Access Unofficial Transcript Term Selector.
-
 // Curl request.
-$c = curl_init('https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: mycoast.cccd.edu:443'));
-curl_setopt($c, CURLOPT_HEADER, true); //include headers in http data
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0); // we could follow the location, but we would lose control...
-$http_data = curl_exec($c);
+$url = 'https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran';
+$options['header'] = true;
+$data = performRequest($url, $cookie_file_path, $options); // we could follow_location,
+                                                           // but we would lose control...
+unset($options['header']);
 
 // Get the Location response header.
-$curl_info = curl_getinfo($c);
-$headers = substr($http_data, 0, $curl_info["header_size"]); //split out header
+$curl_info = $data["info"];
+$headers = substr($data["response"], 0, $curl_info["header_size"]); //split out header
 preg_match("!\r\n(?:Location|URI): *(.*?) *\r\n!", $headers, $matches);
 $url = $matches[1]; // Location to follow next...
-curl_close($c);
 
 // 3.c. Access Unofficial Transcript Term Selector.
-$c = curl_init($url);
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_REFERER, 'https://mycoast.cccd.edu/cp/render.uP?uP_root=root&uP_sparam=activeTab&activeTab=u12l1s2&uP_tparam=frm&frm=backLinked&uP_tparam=utf&utf=&https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
-curl_exec($c);
-curl_close($c);
+$options['headers'] = false;
+$options['follow_location'] = true;
+$options['referer'] = 'https://mycoast.cccd.edu/cp/render.uP?uP_root=root&uP_sparam=activeTab&activeTab=u12l1s2&uP_tparam=frm&frm=backLinked&uP_tparam=utf&utf=&https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 3.d. Access Unofficial Transcript Term Selector.
-$c = curl_init('https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: bannerlsp.cccd.edu:443'));
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_REFERER, 'https://mycoast.cccd.edu/cp/render.uP?uP_root=root&uP_sparam=activeTab&activeTab=u11l1s2&uP_tparam=frm&frm=backLinked&uP_tparam=utf&utf=&https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
-curl_exec($c);
-curl_close($c);
+$url = 'https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran';
+$options['referer'] = 'https://mycoast.cccd.edu/cp/render.uP?uP_root=root&uP_sparam=activeTab&activeTab=u11l1s2&uP_tparam=frm&frm=backLinked&uP_tparam=utf&utf=&https://mycoast.cccd.edu/cp/ip/login?sys=sctssb&url=https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran';
+$data = performRequest($url, $cookie_file_path, $options);
 
 // 4. Access the Unofficial Transcripts.
 
 // Set up the POST Info.
-$transcriptPostinfo = "levl=&tprt=WEB"; // levl=OC only for OCC
+$transcriptPostInfo = "levl=&tprt=WEB"; // levl=OC only for OCC
 
 // Perform the curl request.
-$c = curl_init('https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTran');
-curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($c, CURLOPT_VERBOSE, true);
-curl_setopt($c, CURLOPT_NOBODY, false);
-curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-curl_setopt($c, CURLOPT_REFERER, 'https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran');
-curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($c, CURLOPT_FOLLOWLOCATION, 0);
-curl_setopt($c, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($c, CURLOPT_POST, 1);
-curl_setopt($c, CURLOPT_POSTFIELDS, $transcriptPostinfo);
-$transcriptsHTML = curl_exec($c);
-curl_close($c);
+$url = 'https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTran';
+$options['referer'] = 'https://bannerlsp.cccd.edu/pls/PROD/bwskotrn.P_ViewTermTran';
+$options['post'] = $transcriptPostInfo;
+$options['follow_location'] = false;
+$data = performRequest($url, $cookie_file_path, $options);
+$transcriptsHTML = $data['response'];
 
 // Close the cookie file.
 fclose($cookie_file);
@@ -222,21 +122,21 @@ $transcriptsHTML = str_replace('HREF="' . $HREF_Banner_Change[1], 'href="' . $ba
 $dom = new DOMDocument();
 if(@$dom->loadHTML($transcriptsHTML))
 {
-    while (($r = $dom->getElementsByTagName("script")) && $r->length) {
-            $r->item(0)->parentNode->removeChild($r->item(0));
-    }
+  while (($r = $dom->getElementsByTagName("script")) && $r->length) {
+    $r->item(0)->parentNode->removeChild($r->item(0));
+  }
 
-    while (($r = $dom->getElementsByTagName("img")) && $r->length) {
-            $r->item(0)->parentNode->removeChild($r->item(0));
-    }
+  while (($r = $dom->getElementsByTagName("img")) && $r->length) {
+    $r->item(0)->parentNode->removeChild($r->item(0));
+  }
 
-    // 5.c. Get rid of div element with a specific class.
-    $xpath = new DOMXpath($dom);
-    $nodes = $xpath->query( "//div[contains(concat(' ', @class, ' '), ' headerwrapperdiv ')]");
-    foreach( $nodes as $node) {
-	    $node->parentNode->removeChild( $node);
-	}
-    
+  // 5.c. Get rid of div element with a specific class.
+  $xpath = new DOMXpath($dom);
+  $nodes = $xpath->query( "//div[contains(concat(' ', @class, ' '), ' headerwrapperdiv ')]");
+  foreach( $nodes as $node) {
+    $node->parentNode->removeChild( $node);
+  }
+
 	$transcriptsHTML = $dom->saveHTML();
 }
 
@@ -249,5 +149,63 @@ $transcriptsHTML = $styleFix . $transcriptsHTML;
 
 // 6. Return back (print) the transcripts.
 echo $transcriptsHTML;
+
+/* USEFUL METHODS
+*******************************************/
+
+/**
+* Performs a cURL request to a given url and returns the response headers and content.
+*/
+function performRequest($url, $cookie_file_path, $options = NULL) {
+  $c = curl_init($url);
+  curl_setopt($c, CURLOPT_NOBODY, false);
+  curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($c, CURLOPT_COOKIEJAR, $cookie_file_path);
+  curl_setopt($c, CURLOPT_COOKIEFILE, $cookie_file_path);
+  curl_setopt($c, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+
+  if ($options != NULL) {
+    foreach ($options as $option => $value) {
+      if ($value != NULL) {
+        switch ($option) {
+          case 'host':
+            curl_setopt($c, CURLOPT_HTTPHEADER, array('Host: ' . $value));
+          break;
+          case 'referer':
+            curl_setopt($c, CURLOPT_REFERER, $value);
+          break;
+          case 'post':
+            curl_setopt($c, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($c, CURLOPT_POST, 1);
+            curl_setopt($c, CURLOPT_POSTFIELDS, $value);
+          break;
+          case 'follow_location':
+            if ($value) {
+              curl_setopt($c, CURLOPT_FOLLOWLOCATION, 1);
+            }
+          break;
+          case 'header':
+            if ($value) {
+              curl_setopt($c, CURLOPT_HEADER, true); //include headers in http data
+            }
+        }
+      }
+    }
+  }
+
+  $response = curl_exec($c);
+  $curl_info = curl_getinfo($c);
+
+  $data = array(
+    'response' => $response,
+    'info' => $curl_info
+  );
+
+  curl_close($c);
+
+  return $data;
+}
 
 ?>
